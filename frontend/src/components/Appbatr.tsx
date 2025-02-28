27a7047d6403bc48a71e8b5e9cdbdbf8f4b64e90
Vivek] <vcchaudhary777@gmail.com>
2025-02-28 11:55:29 +0530

    some changes in the width of appbar

--- frontend/src/components/Appbatr.tsx
+++ frontend/src/components/Appbatr.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 import { Avatar } from "./BlogSingle"
 
 export const Appbar=()=>{
-    return <div className="flex justify-between items-center border-b  p-4  md:w-screen">
+    return <div className="flex justify-between items-center border-b  p-4  md:w-[100%]">
        <Link to={"/blogs"}>
         <div className="cursor-pointer">
             Medium
@@ -10,7 +10,7 @@ export const Appbar=()=>{
        </Link>
         <div>
         <Link to={"/publish"}>
-        <button type="button" className="cursor-pointer text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-3 me-2 mb-2">Add</button>
+        <button type="button" className="cursor-pointer text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-3  mb-2">Add</button>
         </Link>
        
             <Avatar authorName="Vivek"/>
